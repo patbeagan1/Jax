c4b8e212558cbee06a57363ced75ee59d3beae0f
patbeagan1 <pbeagan1@gmail.com>
2017-01-14 19:17:29 -0500

    making the server pull in data from the file

--- try-meteor/server/main.js
+++ try-meteor/server/main.js
@@ -3,3 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.startup(function () {
+    console.log('starting up');
+	var data = Assets.getText('HelloWorld.java');
+	console.log(data);
+});
